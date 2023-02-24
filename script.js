039e839d2a4812d2f3a5d03c5226d5ad8996d730
breatheintheair <polinamorozova5133@gmail.com>
2023-02-24 19:06:34 +0200

    init repository and added files

---
+++ script.js
@@ -0,0 +1,6 @@
+function navToggle() {
+    var headerAuth = document.getElementById("headerAuth");
+    var close = document.getElementById("close");
+    headerAuth.classList.toggle('_active');
+  }
+  
